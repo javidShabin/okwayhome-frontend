bfdb2d52cf312811b4da42178a7ad3163b444af0
javid shabin <javid.main.act@gmail.com>
2024-08-20 01:21:05 +0530

    Create catagori datas

---
+++ src/components/FilteredData.jsx
@@ -0,0 +1,34 @@
+import { bed, chair, sofa, table, teapoy } from "../assets";
+
+const itemData = [
+    {
+        id: 1,
+        imgSrc: bed,
+        category: "Bed",
+        name: "Bed"
+    },
+    {
+        id: 2,
+        imgSrc: sofa,
+        category: "sofa",
+        name: "sofa"
+    },
+    {
+        id: 3,
+        imgSrc: table,
+        category: "Table",
+        name: "Table"
+    },
+    {
+        id: 4,
+        imgSrc: teapoy,
+        category: "Teapoy",
+        name: "Teapoy"
+    },
+    {
+        id: 5,
+        imgSrc: chair,
+        category: "Chair",
+        name: "Chair"
+    },
+]
