c645283496db362e452ab756d43ff12ad88722f2
javid shabin <javid.main.act@gmail.com>
2024-08-20 01:20:44 +0530

    Create catagory data

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,5 +1,15 @@
 import hero from "./home.jpeg"
+import bed from "./bed.jpeg"
+import sofa from "./sofa.jpeg"
+import table from "./table.jpeg"
+import teapoy from "./teapoy.jpeg"
+import chair from "./chair.webp"
 
 export{
-    hero
+    hero,
+    bed,
+    sofa,
+    table,
+    teapoy,
+    chair
 }
