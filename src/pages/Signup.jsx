1532f251b19376945839f8b5019926a7ff58cd3d
javid shabin <javid.main.act@gmail.com>
2024-10-16 01:26:39 +0530

    Solve registration problrm

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,31 +3,32 @@ import { axiosInstants } from "../config/axiosInstants";
 import { Link, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
+
 export default function SignupPage() {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-
   const onSubmit = async (data) => {
     try {
       const response = await axiosInstants({
         method: "POST",
         url: "/user/register",
-        data
-      })
-      console.log(response)
+        data,
+      });
+      toast.success("Create user")
       navigate('/')
-      toast.success("Registered")
     } catch (error) {
-      
+      toast.error(error.response.data.message)
+      console.log(error);
     }
   };
-  
+
   return (
     <div className="flex justify-center items-center h-[87vh] bg-gray-100">
+      <img src="" className="hidden sm:block" />
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="w-full max-w-md p-8 bg-white rounded-lg shadow-lg flex flex-col"
@@ -40,61 +41,43 @@ export default function SignupPage() {
           className="mb-4 p-3 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
           type="text"
           placeholder="Name"
-          {...register("name", { required: "Name is required" })}
+          {...register("name", { required: true })}
         />
-        {errors.name && (
-          <span className="text-red-500 mb-4">{errors.name.message}</span>
-        )}
 
         <input
           className="mb-4 p-3 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
           type="email"
           placeholder="Email"
-          {...register("email", { required: "Email is required" })}
+          {...register("email", { required: true })}
         />
-        {errors.email && (
-          <span className="text-red-500 mb-4">{errors.email.message}</span>
-        )}
 
         <input
           className="mb-4 p-3 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
           type="password"
           placeholder="Password"
-          {...register("password", { required: "Password is required" })}
+          {...register("password", { required: true })}
         />
-        {errors.password && (
-          <span className="text-red-500 mb-4">{errors.password.message}</span>
-        )}
 
         <input
           className="mb-4 p-3 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
           type="password"
           placeholder="Confirm Password"
-          {...register("confirmPassword", {
-            required: "Confirm Password is required",
-          })}
+          {...register("conformPassword", { required: true })}
         />
-        {errors.confirmPassword && (
-          <span className="text-red-500 mb-4">
-            {errors.confirmPassword.message}
-          </span>
-        )}
 
         <input
           className="mb-4 p-3 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
           type="tel"
           placeholder="Phone"
-          {...register("phone", {
-            required: "Phone number is required",
-            pattern: { value: /^[0-9]+$/, message: "Invalid phone number" },
-          })}
+          {...register("phone", { required: true })}
         />
-        {errors.phone && (
-          <span className="text-red-500 mb-4">{errors.phone.message}</span>
+
+        {errors.exampleRequired && (
+          <span className="text-red-500 mb-4">This field is required</span>
         )}
 
         <p className="text-gray-500">
-          Already have an account?{" "}
+          Already have account?{" "}
           <Link to={"/login"}>
             <span className="text-orange-400">Login</span>
           </Link>
